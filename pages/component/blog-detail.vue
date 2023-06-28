e74ca97d080036be55770dd42665adf07160a94d
songyanjie <songyanjie@yunjizhi.cn>
2023-06-28 17:35:16 +0800

    feat：新增分享好友和朋友圈功能

--- pages/component/blog-detail.vue
+++ pages/component/blog-detail.vue
@@ -18,8 +18,10 @@
 		onReady
 	} from '@dcloudio/uni-app';
 	let detaildata = ref({})
+	let detail_id = ref(0)
 	onLoad((options) => {
 		let blogId = options.id; // 获取传递的博客id参数
+		detail_id = options.id
 		blogId = blogId.toString(); // 将 id 转换为字符串类型
 		uni.request({
 			url: 'https://api.suxin23.cn/blog/detail',
@@ -28,7 +30,7 @@
 				id: blogId
 			},
 			success: (res) => {
-				console.log(res.data.rows[0].content)
+				console.log(res.data.rows[0].id)
 				detaildata.value = res.data.rows[0]
 				// 设置导航栏标题
 				uni.setNavigationBarTitle({
@@ -53,13 +55,35 @@
 		const formattedDate = `${year}-${padZero(month)}-${padZero(day)}`;
 		const formattedTime = `${padZero(hour)}:${padZero(minute)}:${padZero(second)}`;
 
-		return  formattedDate +' '+ formattedTime
+		return formattedDate + ' ' + formattedTime
 	};
 
 	// 辅助函数，用于补零
 	const padZero = (value) => {
 		return value < 10 ? `0${value}` : value;
 	};
+
+	const onShareAppMessage = (() => {
+		const promise = new Promise(resolve => {
+			setTimeout(() => {
+				resolve({
+					title: `文章分享-${detaildata.value.title}`
+				})
+			}, 2000)
+		})
+		return {
+			title: `文章分享-${detaildata.value.title}`,
+			path: `/pages/component/blog-detail?id=${detaildata.value.id}`,
+			promise
+		}
+	})
+	const onShareTimeline = (() => {
+		return {
+			title: `文章分享-${detaildata.value.title}`,
+			query: `/pages/component/blog-detail?id=${detaildata.value.id}`,
+
+		}
+	})
 </script>
 
 <style>
